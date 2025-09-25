b98d792618af5412b929e5d3756908817abaac1a
YellowKidokc <17890867+YellowKidokc@users.noreply.github.com>
2025-09-24 22:32:56 -0500

    Goods
    Good

--- run-admin-setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Relaunch this script as admin if not elevated
-net session >nul 2>&1
-if %errorlevel% NEQ 0 (
-  powershell -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%~f0'"
-  exit /b
-)
-
-call setup-gallery.bat gallery
